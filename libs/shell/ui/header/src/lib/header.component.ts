98bc45466e2767c54256e629a014fd5c24ab0b27
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-13 17:55:06 -0300

    feat(shell): add shell-ui-header lib

---
+++ libs/shell/ui/header/src/lib/header.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gg-header',
+  standalone: true,
+  imports: [],
+  template: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
