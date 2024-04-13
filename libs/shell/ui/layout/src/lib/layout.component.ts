f921a7a665d286200d45a5a9c87dc8694a62884d
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-13 17:48:27 -0300

    feat(shell): add shell-ui-layout lib

---
+++ libs/shell/ui/layout/src/lib/layout.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gg-layout',
+  standalone: true,
+  imports: [],
+  template: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LayoutComponent {}
