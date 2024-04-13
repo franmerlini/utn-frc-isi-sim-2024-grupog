947e19733a0f67bec99eda142800acbdd4f7985d
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-13 20:31:03 -0300

    feat(shell): add shell layout template

--- libs/shell/ui/layout/src/lib/layout.component.ts
+++ libs/shell/ui/layout/src/lib/layout.component.ts
@@ -1,10 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from '@grupog/libs/shell/ui/header';
 
 @Component({
   selector: 'gg-layout',
   standalone: true,
-  imports: [],
-  template: ``,
+  imports: [HeaderComponent, RouterOutlet],
+  template: `
+    <div class="h-screen flex flex-column">
+      <gg-header />
+
+      <main class="flex-1 flex justify-content-center py-8 px-4">
+        <router-outlet />
+      </main>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {}
