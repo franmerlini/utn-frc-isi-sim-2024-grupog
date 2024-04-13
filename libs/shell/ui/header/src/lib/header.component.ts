96a63703733a8a3fc4193a48efe2e8d63e6b47e5
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-13 18:29:01 -0300

    feat(shell): add header template

--- libs/shell/ui/header/src/lib/header.component.ts
+++ libs/shell/ui/header/src/lib/header.component.ts
@@ -1,10 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+
 @Component({
   selector: 'gg-header',
   standalone: true,
-  imports: [],
-  template: ``,
+  imports: [ToolbarModule, ButtonModule],
+  template: `
+    <p-toolbar>
+      <div class="p-toolbar-group-start">
+        <p-button label="TP2"></p-button>
+      </div>
+
+      <div class="p-toolbar-group-center">
+        <p-button label="Uniforme" [link]="true"></p-button>
+        <p-button label="Exponencial" [link]="true"></p-button>
+        <p-button label="Normal" [link]="true"></p-button>
+      </div>
+
+      <div class="p-toolbar-group-end">
+        <p-button label="Github" icon="pi pi-github"></p-button>
+      </div>
+    </p-toolbar>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
