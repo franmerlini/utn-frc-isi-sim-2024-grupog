95d9467bc54fe3f1c811d98e990e55dc96fb9bbf
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-15 19:52:48 -0300

    feat(tp2): add animations provider

--- apps/tp2/src/app/app.config.ts
+++ apps/tp2/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(ROUTES)],
+  providers: [provideRouter(ROUTES), provideAnimationsAsync()],
 };
