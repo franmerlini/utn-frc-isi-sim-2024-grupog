e8ae28f289231f14745fa4c277e4326161cef12a
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-15 19:55:09 -0300

    feat(shell): add nav item list

--- libs/shell/ui/layout/src/lib/layout.component.ts
+++ libs/shell/ui/layout/src/lib/layout.component.ts
@@ -1,15 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuItem } from 'primeng/api';
+
 import { HeaderComponent } from '@grupog/libs/shell/ui/header';
+import { SidebarComponent } from '@grupog/libs/shell/ui/sidebar';
 
 @Component({
   selector: 'gg-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent],
   template: `
     <div class="h-screen flex flex-col">
-      <gg-header />
+      <gg-header [navItems]="navItems()" (openSidebar)="openSidebar.set(true)" />
+
+      <gg-sidebar [navItems]="navItems()" [(openSidebar)]="openSidebar" />
 
       <main class="flex-1 flex justify-center px-2 pt-8">
         <div class="container">
@@ -20,4 +25,23 @@ import { HeaderComponent } from '@grupog/libs/shell/ui/header';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  openSidebar = signal(false);
+  navItems = signal<MenuItem[]>([
+    {
+      label: 'Inicio',
+      icon: 'pi pi-home',
+      routerLink: '/',
+    },
+    {
+      label: 'Enunciado',
+      icon: 'pi pi-list-check',
+      routerLink: 'enunciado',
+    },
+    {
+      label: 'Simulación',
+      icon: 'pi pi-play',
+      routerLink: 'simulacion',
+    },
+  ]);
+}
