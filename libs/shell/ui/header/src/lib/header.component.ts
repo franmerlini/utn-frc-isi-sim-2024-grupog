606b553a95036e95fcac256b860c9c433af1fc6a
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-15 19:54:43 -0300

    feat(shell): add breakpoint validation to render nav items

--- libs/shell/ui/header/src/lib/header.component.ts
+++ libs/shell/ui/header/src/lib/header.component.ts
@@ -1,19 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, Component, inject, input, output } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
 
+import { map } from 'rxjs';
+
+import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+
+import { SidebarComponent } from '@grupog/libs/shell/ui/sidebar';
 
 @Component({
   selector: 'gg-header',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, RouterLink],
+  imports: [ToolbarModule, ButtonModule, RouterLink, SidebarComponent],
   template: `
     <p-toolbar>
       <div class="p-toolbar-group-start">
-        <p-button label="Inicio" [link]="true" routerLink="/"></p-button>
-        <p-button label="Enunciado" [link]="true" routerLink="enunciado"></p-button>
-        <p-button label="Simulación" [link]="true" routerLink="simulacion"></p-button>
+        @if (isSmallScreen()) {
+        <p-button icon="pi pi-bars" [text]="true" (click)="openSidebar.emit()"></p-button>
+        } @else { @for (navItem of navItems(); track navItem.label) {
+        <p-button [label]="navItem.label" [link]="true" [routerLink]="navItem.routerLink"></p-button>
+        } }
       </div>
 
       <div class="p-toolbar-group-end">
@@ -25,4 +34,13 @@ import { ToolbarModule } from 'primeng/toolbar';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  navItems = input.required<MenuItem[]>();
+  openSidebar = output();
+
+  isSmallScreen = toSignal(
+    inject(BreakpointObserver)
+      .observe('(max-width: 640px)')
+      .pipe(map(({ matches }) => matches))
+  );
+}
