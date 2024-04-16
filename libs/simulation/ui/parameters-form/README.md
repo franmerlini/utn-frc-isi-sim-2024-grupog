a5949c84eb2b41bc3d904e99186d8111d9d937f6
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-16 11:44:24 -0300

    feat(simulation): add simulation-ui-parameters-form lib

---
+++ libs/simulation/ui/parameters-form/README.md
@@ -0,0 +1,7 @@
+# simulation-ui-parameters-form
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test simulation-ui-parameters-form` to execute the unit tests.
---
+++ libs/simulation/ui/parameters-form/src/lib/parameters-form.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gg-parameters-form',
+  standalone: true,
+  imports: [],
+  template: `<p>parameters-form works!</p>`,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ParametersFormComponent {}
