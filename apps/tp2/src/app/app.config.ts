c0c2d74b7a85127e9e089900c795659b085e3d60
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-21 13:34:14 -0300

    feat(tp2): add root store config

--- apps/tp2/src/app/app.config.ts
+++ apps/tp2/src/app/app.config.ts
@@ -3,8 +3,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 
 import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { CustomSerializer, ROOT_EFFECTS, ROOT_REDUCERS, RouterFeatureKey } from '@grupog/libs/shared/data-access/store';
 
 import { ROUTES } from './app.routes';
 
@@ -12,8 +15,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(ROUTES),
     provideAnimationsAsync(),
-    provideStore(),
-    provideEffects(),
+    provideStore(ROOT_REDUCERS),
+    provideEffects(ROOT_EFFECTS),
+    provideRouterStore({
+      stateKey: RouterFeatureKey,
+      serializer: CustomSerializer,
+    }),
     provideStoreDevtools({ logOnly: !isDevMode() }),
   ],
 };
