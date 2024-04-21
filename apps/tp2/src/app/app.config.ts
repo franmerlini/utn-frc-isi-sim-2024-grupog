e9ab584eed4c4796e96d0772844f8f7437cc91b4
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-21 13:21:04 -0300

    chore(tp2): add ngrx store and effects

--- apps/tp2/src/app/app.config.ts
+++ apps/tp2/src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(ROUTES), provideAnimationsAsync()],
+  providers: [
+    provideRouter(ROUTES),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+  ],
 };
