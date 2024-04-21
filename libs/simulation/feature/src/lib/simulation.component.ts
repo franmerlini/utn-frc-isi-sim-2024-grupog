57e76628c32f50b45a8dae5be93032ed7096aa54
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-21 16:03:56 -0300

    feat(simulation): add randoms table and runSimulation action dispatch

--- libs/simulation/feature/src/lib/simulation.component.ts
+++ libs/simulation/feature/src/lib/simulation.component.ts
@@ -1,23 +1,35 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 
 import { ToastActions } from '@grupog/libs/shared/data-access/store';
 import { Simulation } from '@grupog/libs/shared/models';
+import { SimulationActions, SimulationFeature } from '@grupog/libs/simulation/data-access/store';
 import { ParametersFormComponent } from '@grupog/libs/simulation/ui/parameters-form';
+import { RandomsTableComponent } from '@grupog/libs/simulation/ui/randoms-table';
 
 @Component({
   selector: 'gg-simulation',
   standalone: true,
-  imports: [ParametersFormComponent],
-  template: ` <gg-parameters-form (simulate)="onSimulate($event)" (formError)="onFormError($event)" /> `,
+  imports: [ParametersFormComponent, AsyncPipe, RandomsTableComponent],
+  template: `
+    <div class="flex flex-col gap-8">
+      <gg-parameters-form (simulate)="onSimulate($event)" (formError)="onFormError($event)" />
+
+      @if(randoms$ | async; as randoms) { @if(randoms.length) {
+      <gg-randoms-table [randoms]="randoms" />
+      } }
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SimulationComponent {
   #store = inject(Store);
+  randoms$ = this.#store.select(SimulationFeature.selectRandomNumbers);
 
-  onSimulate(simulation: Simulation): void {
-    console.log(simulation);
+  onSimulate(parameters: Simulation): void {
+    this.#store.dispatch(SimulationActions.runSimulation({ parameters }));
   }
 
   onFormError(message: string): void {
