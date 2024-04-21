2ab1684b7a91d39b2d25e3e2fa00b1713e3c01f9
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-21 17:20:46 -0300

    refactor(shared): add export

--- libs/shared/util/src/lib/random-generators/index.ts
+++ libs/shared/util/src/lib/random-generators/index.ts
@@ -1,3 +1,4 @@
 export * from './generate-exponential-random.util';
 export * from './generate-normal-random.util';
 export * from './generate-uniform-random.util';
+export * from './truncate-decimals.util';
