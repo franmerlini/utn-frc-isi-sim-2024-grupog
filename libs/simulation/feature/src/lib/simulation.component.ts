7bc8d7e484730260db954f589edf537f8d83cad3
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-21 17:22:05 -0300

    feat(simulation): add invervals$ selector and IntervalTable component

--- libs/simulation/feature/src/lib/simulation.component.ts
+++ libs/simulation/feature/src/lib/simulation.component.ts
@@ -6,19 +6,22 @@ import { Store } from '@ngrx/store';
 import { ToastActions } from '@grupog/libs/shared/data-access/store';
 import { Simulation } from '@grupog/libs/shared/models';
 import { SimulationActions, SimulationFeature } from '@grupog/libs/simulation/data-access/store';
+import { IntervalsTableComponent } from '@grupog/libs/simulation/ui/intervals-table';
 import { ParametersFormComponent } from '@grupog/libs/simulation/ui/parameters-form';
 import { RandomsTableComponent } from '@grupog/libs/simulation/ui/randoms-table';
 
 @Component({
   selector: 'gg-simulation',
   standalone: true,
-  imports: [ParametersFormComponent, AsyncPipe, RandomsTableComponent],
+  imports: [ParametersFormComponent, AsyncPipe, RandomsTableComponent, IntervalsTableComponent],
   template: `
     <div class="flex flex-col gap-8">
       <gg-parameters-form (simulate)="onSimulate($event)" (formError)="onFormError($event)" />
 
-      @if(randoms$ | async; as randoms) { @if(randoms.length) {
-      <gg-randoms-table [randoms]="randoms" />
+      @if(randomNumbers$ | async; as randomNumbers) { @if(randomNumbers.length) {
+      <gg-randoms-table [randomNumbers]="randomNumbers" />
+      } } @if(intervals$ | async; as intervals) { @if(intervals.length) {
+      <gg-intervals-table [intervals]="intervals" />
       } }
     </div>
   `,
@@ -26,7 +29,9 @@ import { RandomsTableComponent } from '@grupog/libs/simulation/ui/randoms-table'
 })
 export class SimulationComponent {
   #store = inject(Store);
-  randoms$ = this.#store.select(SimulationFeature.selectRandomNumbers);
+
+  randomNumbers$ = this.#store.select(SimulationFeature.selectRandomNumbers);
+  intervals$ = this.#store.select(SimulationFeature.selectIntervals);
 
   onSimulate(parameters: Simulation): void {
     this.#store.dispatch(SimulationActions.runSimulation({ parameters }));
