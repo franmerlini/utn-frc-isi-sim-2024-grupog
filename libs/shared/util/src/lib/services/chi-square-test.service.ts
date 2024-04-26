4aa7fc0e40f50db4493e7b381be2aa5a7d7ec382
Francisco Merlini <merlinifrancisco@gmail.com>
2024-04-26 12:58:42 -0300

    feat(shared): add ChiSquareTest service

---
+++ libs/shared/util/src/lib/services/chi-square-test.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { ChiSquareTestInterval, Interval } from '@grupog/libs/shared/models';
+import { truncateDecimals } from '../random-generators';
+
+@Injectable()
+export class ChiSquareTestService {
+  private MIN_OBSERVED_FREQUENCY = 5;
+
+  generateIntervals(intervals: Interval[]): Observable<ChiSquareTestInterval[]> {
+    const groupedIntervals: ChiSquareTestInterval[] = [];
+    let currentGroup: Interval[] = [];
+    let currentExpectedFrequency = 0;
+    let currentObservedFrequency = 0;
+    let lowerBound = 0;
+    let accumulatedC = 0;
+
+    for (const interval of intervals) {
+      if (currentGroup.length === 0) {
+        lowerBound = interval.lowerBound;
+      }
+
+      currentGroup.push(interval);
+
+      currentExpectedFrequency += interval.expectedFrequency;
+      currentObservedFrequency += interval.observedFrequency;
+
+      if (currentObservedFrequency >= this.MIN_OBSERVED_FREQUENCY) {
+        const upperBound = currentGroup[currentGroup.length - 1].upperBound;
+
+        const c = (currentExpectedFrequency - currentObservedFrequency) ** 2 / currentExpectedFrequency;
+
+        accumulatedC += c;
+
+        const chiSquareTestInterval: ChiSquareTestInterval = {
+          lowerBound,
+          upperBound,
+          expectedFrequency: truncateDecimals(currentExpectedFrequency, 4),
+          observedFrequency: truncateDecimals(currentObservedFrequency, 4),
+          c: truncateDecimals(c, 4),
+          accumulatedC: truncateDecimals(accumulatedC, 4),
+        };
+
+        groupedIntervals.push(chiSquareTestInterval);
+
+        currentGroup = [];
+        currentExpectedFrequency = 0;
+        currentObservedFrequency = 0;
+      }
+    }
+
+    if (currentGroup.length > 0) {
+      if (groupedIntervals.length > 0) {
+        const lastGroup = groupedIntervals[groupedIntervals.length - 1];
+        lastGroup.upperBound = currentGroup[currentGroup.length - 1].upperBound;
+        lastGroup.expectedFrequency += currentExpectedFrequency;
+        lastGroup.observedFrequency += currentObservedFrequency;
+
+        const c = (lastGroup.expectedFrequency - lastGroup.observedFrequency) ** 2 / lastGroup.expectedFrequency;
+        accumulatedC += c;
+
+        lastGroup.c = truncateDecimals(c, 4);
+        lastGroup.accumulatedC = truncateDecimals(accumulatedC, 4);
+      } else {
+        const upperBound = currentGroup[currentGroup.length - 1].upperBound;
+
+        const c = (currentExpectedFrequency - currentObservedFrequency) ** 2 / currentExpectedFrequency;
+        accumulatedC += c;
+
+        const chiSquareTestInterval: ChiSquareTestInterval = {
+          lowerBound,
+          upperBound,
+          expectedFrequency: truncateDecimals(currentExpectedFrequency, 4),
+          observedFrequency: truncateDecimals(currentObservedFrequency, 4),
+          c: truncateDecimals(c, 4),
+          accumulatedC: truncateDecimals(accumulatedC, 4),
+        };
+
+        groupedIntervals.push(chiSquareTestInterval);
+      }
+    }
+
+    return of(groupedIntervals);
+  }
+}
--- libs/shared/util/src/lib/services/index.ts
+++ libs/shared/util/src/lib/services/index.ts
@@ -1,2 +1,3 @@
+export * from './chi-square-test.service';
 export * from './simulation.service';
 export * from './toast.service';
