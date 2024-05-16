7352627a0f12f4f6c6560790d8beb82756dda117
Francisco Merlini <merlinifrancisco@gmail.com>
2024-05-15 22:17:41 -0300

    feat(monte-carlo-simulation): add monte-carlo-simulation-ui-parameters-form lib

---
+++ libs/monte-carlo-simulation/ui/parameters-form/README.md
@@ -0,0 +1,7 @@
+# monte-carlo-simulation-ui-parameters-form
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test monte-carlo-simulation-ui-parameters-form` to execute the unit tests.
---
+++ libs/monte-carlo-simulation/ui/parameters-form/src/lib/parameters-form.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gg-parameters-form',
+  standalone: true,
+  imports: [],
+  template: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ParametersFormComponent {}
