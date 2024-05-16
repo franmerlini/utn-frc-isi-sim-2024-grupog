9f13f60b100eedd8abe2217ebcef0b03cfda2ed2
Francisco Merlini <merlinifrancisco@gmail.com>
2024-05-15 21:58:14 -0300

    feat(tp3): add root and 'enunciado' routes

--- apps/tp3/src/app/app.routes.ts
+++ apps/tp3/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Route } from '@angular/router';
 
-export const ROUTES: Route[] = [];
+import { LayoutComponent } from '@grupog/libs/shell/ui/layout';
+
+export const ROUTES: Route[] = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@grupog/libs/home/feature').then((c) => c.HomeComponent),
+        data: { theme: 'TP3 - Modelos de simulación estáticos', dueDate: new Date('2024-05-15') },
+      },
+      {
+        path: 'enunciado',
+        loadComponent: () => import('@grupog/libs/assignment/feature').then((c) => c.AssignmentComponent),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
