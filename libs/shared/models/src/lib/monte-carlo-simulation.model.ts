1e64dee2bf98b2c187ba96e46654673cdcb144c6
Francisco Merlini <merlinifrancisco@gmail.com>
2024-05-17 18:55:03 -0300

    refactor(shared): add new properties

--- libs/shared/models/src/lib/monte-carlo-simulation.model.ts
+++ libs/shared/models/src/lib/monte-carlo-simulation.model.ts
@@ -1,6 +1,9 @@
 import { DemandDistributionItem } from './demand-distribution-item.model';
+import { Policy } from './policy.model';
 
 export type MonteCarloSimulation = {
+  policy: Policy;
+  orderAmount: number;
   n: number;
   purchasePrice: number;
   sellingPrice: number;
