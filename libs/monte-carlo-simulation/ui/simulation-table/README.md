ab4a5fe7071b48d453476a69173a13c849bc313a
Francisco Merlini <merlinifrancisco@gmail.com>
2024-05-17 02:17:25 -0300

    feat(monte-carlo-simulation): add monte-carlo-simulation-ui-simulation-table lib

---
+++ libs/monte-carlo-simulation/ui/simulation-table/README.md
@@ -0,0 +1,7 @@
+# monte-carlo-simulation-ui-simulation-table
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test monte-carlo-simulation-ui-simulation-table` to execute the unit tests.
---
+++ libs/monte-carlo-simulation/ui/simulation-table/src/lib/simulation-table.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gg-simulation-table',
+  standalone: true,
+  imports: [],
+  template: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SimulationTableComponent {}
