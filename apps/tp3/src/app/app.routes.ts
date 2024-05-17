8ae2c032c3b1fd262c71e23c1b08ea6d7a85d514
Francisco Merlini <merlinifrancisco@gmail.com>
2024-05-17 02:13:03 -0300

    feat(tp3): add missing providers for 'simulacion' route

--- apps/tp3/src/app/app.routes.ts
+++ apps/tp3/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Route } from '@angular/router';
 
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+
+import {
+  MonteCarloSimulationEffects,
+  MonteCarloSimulationFeature,
+} from '@grupog/libs/monte-carlo-simulation/data-access/store';
+import { MonteCarloSimulationService } from '@grupog/libs/shared/util';
 import { LayoutComponent } from '@grupog/libs/shell/ui/layout';
 
 export const ROUTES: Route[] = [
@@ -20,6 +28,11 @@ export const ROUTES: Route[] = [
         path: 'simulacion',
         loadComponent: () =>
           import('@grupog/libs/monte-carlo-simulation/feature').then((c) => c.MonteCarloSimulationComponent),
+        providers: [
+          MonteCarloSimulationService,
+          provideState(MonteCarloSimulationFeature),
+          provideEffects(MonteCarloSimulationEffects),
+        ],
       },
     ],
   },
